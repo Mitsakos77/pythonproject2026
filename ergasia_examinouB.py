96157910713df543fd8c7eae5c38381db719acfb
Mitsakos77 <manthosdimitris315@gmail.com>
2026-04-21 16:39:49 +0300

    τάδιο 1: Ορισμός βασικών δομών δεδομένων (tuples, sets, lists)

---
+++ ergasia_examinouB.py
@@ -0,0 +1,16 @@
+club_info_robotics = ("Λέσχη Ρομποτικής", "κ. Ιωάννου", "Δευτέρα")
+club_info_theater = ("Λέσχη Θεάτρου", "κ. Παπαδοπούλου", "Τετάρτη")
+club_info_music = ("Λέσχη Μουσικής", "κ. Νικολάου", "Παρασκευή")
+club_info_sports = ("Λέσχη Αθλητισμού", "κ. Δημητρίου", "Πέμπτη")
+
+students_robotics = {"Νίκος", "Μαρία", "Γιώργος"}
+students_theater = {"Ελένη", "Κώστας", "Μαρία"}
+students_music = {"Άννα", "Δημήτρης"}
+students_sports = {"Γιάννης", "Σοφία", "Νίκος"}
+
+all_clubs = [
+    [club_info_robotics, students_robotics],
+    [club_info_theater, students_theater],
+    [club_info_music, students_music],
+    [club_info_sports, students_sports]
+]
